6e5c82a4d73da8479793d46de0cbc1c24103a960
LucasNithael <lucasnithael0101@gmail.com>
2023-06-22 21:16:31 -0300

    featuremovie component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, {useEffect, useState} from 'react'
 import './App.css'
 import myList from './Tmdb'
 import MovieRow from './components/MovieRow'
+import FeaturedMovie from './components/FeaturedMovie'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   const [movieList, setMovieList] = useState([])
+  const [featureData, setFeaturedData] = useState(null)
 
   useEffect(() => {
     const loadAll = async () => {
+      // lista total de filmes
       let list = await myList.getHomeList()
       setMovieList(list)
+
+      // filme em destaque
+      let orininals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (orininals[0].itens.results.length - 1))
+      let chosen = orininals[0].itens.results[randomChosen]
+
+      console.log(chosen)
     }
 
     loadAll()
@@ -18,6 +28,9 @@ export default () => {
 
   return (
     <div className='page'>
+
+      <FeaturedMovie />
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} itens={item.itens}/>
---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './FeaturedMovie.css'
+
+const FeaturedMovie = () => {
+    return(
+        <div>
+            FILME EM DESTAQUE
+        </div>
+    )
+}
+
+export default FeaturedMovie
